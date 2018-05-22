ced4603a0c2968a88c172d15f5bffca1987af8c1
github username <github email address>
2018-05-21 23:10:37 -0700

    edited to simplify and perform .delete_if method

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,14 +13,7 @@ def first_challenge
     }
   }
 
-  contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        attribute.delete_if {|value| value == "strawberry"}
-      end
-    end
-  end
-
+contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|value| value == "strawberry"}
 
   #remember to return your newly altered contacts hash!
   contacts
